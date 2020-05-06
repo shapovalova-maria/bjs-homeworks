6745940d4a5a6fc99e7a16325703ca7f56460343
Maria Shapovalova <shapovalova.maria.vladimirovna@gmail.com>
2020-05-06 23:49:31 +0200

    Первое решение задач 1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -3,29 +3,36 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
     let totalAmount;
     let bodyCredit;
     let paidMonth;
+    let totalMonth;
+    let yearOfCredit = date.getFullYear();
+    let yearToday = new Date().getFullYear();
     let percentOfCredit;
 
     percent = Number(percent);
     contribution = Number(contribution);
     amount = Number(amount);
-    date = Number(date);
-  
+    // date = date.getFullYear();
+
     if (typeof percent !== 'number') {
         totalAmount = `Параметр процентная ставка содержит неправильное значение ${percent}`;
     } else if (typeof contribution !== 'number') {
         totalAmount = `Параметр сумма первоначального взноса содержит неправильное значение ${contribution}`;
     } else if (typeof amount !== 'number') {
         totalAmount = `Параметр сумма кредита содержит неправильное значение ${amount}`;
-    } else if (typeof date !== 'number') {
-        totalAmount = `Параметр срок содержит неправильное значение ${date}`;
-    } else {
+     } else 
+    //if (typeof date !== 'number') {
+    //     totalAmount = `Параметр срок содержит неправильное значение ${date}`;
+    // } else
+     {
         bodyCredit = amount - contribution;
         percentOfCredit = (percent / 12) / 100;
-        paidMonth = bodyCredit * (percentOfCredit + (percentOfCredit / (Math.pow((1 + percentOfCredit), date) - 1)));
-        totalAmount = Math.round10((amount + paidMonth), -2);
+        totalMonth = (yearOfCredit - yearToday) * 12;
+        paidMonth = bodyCredit * (percentOfCredit + (percentOfCredit / (Math.pow((1 + percentOfCredit), totalMonth) - 1)));
+        totalAmount = bodyCredit + paidMonth;
+       
      }
     
-     return totalAmount;
+     return Math.round10(totalAmount, -2);
 }
 
 function getGreeting(name) {
