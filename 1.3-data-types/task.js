b1bcaabb0e34c9c9d6f20aab9be967fd758d7c09
Maria Shapovalova <shapovalova.maria.vladimirovna@gmail.com>
2020-05-06 22:51:14 +0200

    Первое решение задач 1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,34 @@
+'use strict';
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let totalAmount;
+    let bodyCredit;
+    let paidMonth;
+    let percentOfCredit;
+
+    percent = Number(percent);
+    contribution = Number(contribution);
+    amount = Number(amount);
+    date = Number(date);
+  
+    if (typeof percent !== 'number') {
+        totalAmount = `Параметр процентная ставка содержит неправильное значение ${percent}`;
+    } else if (typeof contribution !== 'number') {
+        totalAmount = `Параметр сумма первоначального взноса содержит неправильное значение ${contribution}`;
+    } else if (typeof amount !== 'number') {
+        totalAmount = `Параметр сумма кредита содержит неправильное значение ${amount}`;
+    } else if (typeof date !== 'number') {
+        totalAmount = `Параметр срок содержит неправильное значение ${date}`;
+    } else {
+        bodyCredit = amount - contribution;
+        percentOfCredit = (percent / 12) / 100;
+        paidMonth = bodyCredit * (percentOfCredit + (percentOfCredit / (Math.pow((1 + percentOfCredit), date) - 1)));
+        totalAmount = Math.round10((amount + paidMonth), -2);
+     }
+    
+     return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    
+    return `Привет, мир! Меня зовут ${name || "Аноним"}`;
 }
