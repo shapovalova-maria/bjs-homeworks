71991440673f56c61ba9c709a9f472dd57400417
Maria Shapovalova <shapovalova.maria.vladimirovna@gmail.com>
2020-05-29 20:16:09 +0200

    Доработка задач

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -1,58 +1,177 @@
 class Weapon {
-    constructor (name, attack, durability, range) {
+    constructor ({'name': name, 'attack': attack, 'durability': durability, 'range': range}) {
     this.name = name;
     this.attack = attack;
     this.durability = durability;
     this.range = range;
+    this.initialDurability = durability;
     }
         
     takeDamage(damage) {
-        this.damage = damage;
-        this.durability = this.durability - this.damage;
+        this.durability = this.durability - damage;
     if (this.durability <= 0) {
         this.durability = 0;
-    } return this.durability;     
+    } return this.durability;
     } 
   
     getDamage() {
-    if (this.durability == 0) {
+
+    if (this.durability === 0) {
         return 0;
-    } else if (this.durability <= durability * 0,3) {
-        this.attack = this.attack / 2;
-    } return this.attack;          
+    } else if (this.durability <= this.initialDurability * 0,3) {
+        return this.attack / 2;
+    } else {
+        return this.attack;
+    }     
     }
 
     isBroken() {
-    return this.durability > 0;
+    return this.durability <= 0;
     }
 }
 
-const sword = new Weapon({
-    name: 'Старый меч',
-    attack: 20,
-    durability: 10,
-    range: 1,
-  });
+class Bow extends Weapon {
 
-  const arm = new Weapon({
+}
+
+class Arm extends Weapon {
+ 
+  
+}
+
+class Blade extends Weapon {
+
+}
+
+class Knife extends Weapon {
+
+}
+
+class Staff extends Weapon {
+
+}
+
+class LongBow extends Bow {
+
+}
+
+class Axe extends Blade {
+
+}
+
+class StormStaff extends Staff {
+
+}
+
+// const sword = new Weapon({
+//     name: 'Старый меч',
+//     attack: 20,
+//     durability: 10,
+//     range: 1,
+//   });
+
+  const arm = new Arm({
     name: 'Рука',
     attack: 1,
     durability: Infinity,
     range: 1,
   });
 
-  const bow = new Weapon({
+  const bow = new Bow({
     name: 'Лук',
     attack: 10,
     durability: 200,
     range: 3,
   });
 
-// let hand = new Weapon('Рука', 1, Infinity, 1);
-// let archery = new Weapon('Лук', 10, 200, 3);
-let sword = new Weapon('Меч', 25, 500, 1);
-let blade = new Weapon('Нож', 5, 300, 1);
-let stick = new Weapon('Посох',	8, 300, 2);
+  const blade = new Blade({
+    name: 'Меч',
+    attack: 25,
+    durability: 500,
+    range: 1,
+  });
+
+  const knife = new Knife({
+    name: 'Нож',
+    attack: 5,
+    durability: 300,
+    range: 1,
+  });
+
+  const staff = new Staff({
+    name: 'Посох',
+    attack: 8,
+    durability: 300,
+    range: 2,
+  });
+
+  const longBow = new LongBow({
+    name: 'Длинный лук',
+    attack: 15,
+    durability: bow.durability,
+    range: 4,
+  });
+  
+  const axe = new Axe({
+    name: 'Секира',
+    attack: 27,
+    durability: 800,
+    range: blade.range,
+  });
+
+  const stormStaff = new StormStaff({
+    name: 'Посох Бури',
+    attack: 10,
+    durability: staff.durability,
+    range: 3,
+  });
+
+
+class StudentLog {
+  constructor (name) {
+    this.name = name;
+    this.scores = {};
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  addGrade(grade, subject) {
+    if (typeof grade != Number) {
+      return `Вы пытались поставить оценку "${grade}" по предмету "${subject}". Допускаются только числа от 1 до 5.\n`;
+    } else 
+    if (grade > 5 & grade < 1) {
+      return `Вы пытались поставить оценку "${grade}" по предмету "${subject}". Допускаются только числа от 1 до 5.\n`;
+    } else {    
+    this.scores.subject.push(grade);
+    }
+  }
+
+  getAverageBySubject(subject) {
+    this.scores.push(subject);
+    if (this.scores.subject.length === 0) {
+      return 0;
+    } else {
+    let sum = 0;
+    for (let score of this.scores.subject) {
+      sum += score;
+   }  return sum / this.scores.subject.length;}
+
+  }
+
+  getTotalAverage() {
+    let averGrade = getAverageBySubject();
+    if (this.scores.subject.length === 0) {
+      return 0;
+    } else {
+    for (let i = 0; i < averGrade.length; i++) {
+      let averSum = 0;
+      averSum += averGrade[i];
+    } return everSum / averGrade.length;}
+  }
+}
+
 
 
 
