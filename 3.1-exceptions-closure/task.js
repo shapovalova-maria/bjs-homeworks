6f54f0af591bccc1d1fd640d7dc6b9645d436a1a
Maria Shapovalova <shapovalova.maria.vladimirovna@gmail.com>
2020-06-11 21:09:09 +0200

    3.1

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -1,22 +1,21 @@
 
  function parseCount(arg) {
-    if (Number.isNaN(Number.parseInt(arg))) {
-        const err = new Error("Невалидное значение"); 
-        throw err;
-    } return Number.parseInt(arg);
+     const parseArg = Number.parseInt(arg)
+    if (Number.isNaN(parseArg)) {
+       throw new Error("Невалидное значение");
+    } return parseArg;
         
   
 }
 
 function validateCount(arg) {
     try {
-        parseCount(arg);
+    return parseCount(arg);
     }
     catch(err) {
-        return err; 
-   
+        return err;
     }
-    return parseCount(arg);
+    
 }
 
 
@@ -26,35 +25,37 @@ class Triangle {
         this.b = b;
         this.c = c;
 
-        if ((this.a + this.b < this.c) || 
-            (this.a + this.c < this.b) ||
-         (this.b + this.c < this.a)) 
+        if ((a + b < c) || 
+            (a + c < b) ||
+         (b + c < a)) 
         {
-            const err = new Error("Треугольник с такими сторонами не существует"); 
-            throw err;
+            throw new Error("Треугольник с такими сторонами не существует");
         }
     }
 
     getPerimeter() {
-        const p = this.a + this.b + this.c;
-        return p;
+        return this.a + this.b + this.c; 
     }
 
     getArea() {
-        const halfP = getPerimeter() / 2;
+        const halfP = this.getPerimeter() / 2;
         const s = Math.sqrt(halfP * (halfP - this.a) * (halfP - this.b) * (halfP - this.c));
-        return s.toFixed(3);
+        return Number(s.toFixed(3));
     }
 
 }
 
 
  function getTriangle(a, b, c) {
-let triangle = new Triangle(a, b, c);
+    let triangle;
 try {
-   return triangle;    
+   return triangle = new Triangle(a, b, c);
 }
 catch(err) {
-    return err;
+    return triangle = {
+        getPerimeter() {return 'Ошибка! Неправильный треугольник'},
+        getArea() {return 'Ошибка! Неправильный треугольник'}
+    }
  } 
- }
+}
+ 
